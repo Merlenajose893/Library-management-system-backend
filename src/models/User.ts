d12af11277fbd2eef4779c4a2cabcb7b0780cd16
Merlena <merlenajose.2003@gmail.com>
2026-08-10 13:46:13 +0530

    feat: add user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,26 @@
-import { Schema,Document } from "mongoose";
-export interface IUser extends Document{
+import mongoose, { Schema,Document, mongo } from "mongoose";
+export interface IUser{
+name:string;
+email:string;
+password:string;
+createdAt:Date;
+updatedAt:Date;
 
 }
+
+const userSchema=new Schema<IUser>({
+name:{
+type:String,
+required:true
+},
+email:{
+type:String,
+required:true
+},
+password:{
+type:String,
+required:true
+}
+},{timestamps:true})
+
+export const User=mongoose.model("User",userSchema)
